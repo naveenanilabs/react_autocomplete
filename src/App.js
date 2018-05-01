91c796da16f150b58a771f71d6b0072dad11dee1
kav <nk1383886@gmail.com>
2018-05-01 11:23:47 +0530

    add search form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+   query: '',
+ }
+
+
+  handleInputChange = () => {
+   this.setState({
+     query: this.search.value
+   })
+ }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+         <input
+           placeholder="Search for..."
+           ref={input => this.search = input}
+           onChange={this.handleInputChange}
+         />
+         <p>{this.state.query}</p>
+       </form>
       </div>
     );
   }
